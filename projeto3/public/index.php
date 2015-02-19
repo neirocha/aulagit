cbdb4dcf9de3d2a8ec6626aaf77182df7be7e29f
neirocha <saturncomunicacao@hotmail.com>
2015-02-19 11:37:11 -0300

    ALteração de erro na Index

--- projeto3/public/index.php
+++ projeto3/public/index.php
@@ -11,86 +11,84 @@
 
 
  <?php
-//ini_set("display_errors", "0");
+// ini_set("display_errors", "0");
 
 
-if(isset($_GET['pg'])){
+if(isset($_GET['pg'])) {
 
-$pg = $_GET['pg'];
+    $pg = $_GET['pg'];
+
+
+    switch ($pg) {
+        case "Home":
+
+            include "view/home.php";
+
+            break;
+
+        case "home":
+
+            include "view/home.php";
+
+            break;
+
+        case "Empresa":
+
+            require_once "view/empresa.php";
+
+            break;
+
+        case "empresa":
+
+            require_once "view/empresa.php";
+
+            break;
+
+        case "Produtos":
+
+            require_once "view/produtos.php";
+
+            break;
+
+        case "produtos":
+
+            require_once "view/produtos.php";
+
+            break;
+
+        case "Servicos":
+
+            require_once "view/servicos.php";
+
+            break;
+
+        case "servicos":
+
+            require_once "view/servicos.php";
+
+            break;
+
+        case "Contato":
+
+            require_once "view/contato.php";
+
+            break;
+        case "contato":
+
+            require_once "view/contato.php";
+
+
+            break;
+
+        default:
+
+    }
+}else{
+	require_once "view/home.php";
 
 }
 
-
-
-switch ($pg){
-case "Home":
-
-include "view/home.php";
-
-break;
-
-case "home":
-
-include "view/home.php";
-
-break;
-
-case "Empresa":
-
-require_once "view/empresa.php"; 
-
-break;
-
-case "empresa":
-
-require_once "view/empresa.php"; 
-
-break;
-
-case "Produtos":
-
-require_once "view/produtos.php";
-
-break;
-
-case "produtos":
-
-require_once "view/produtos.php";
-
-break;
-
-case "Servicos":
-
-require_once "view/servicos.php";
-
-break;
-
-case "servicos":
-
-require_once "view/servicos.php";
-
-break;
-
-case "Contato":
-
-require_once "view/contato.php";
-
-break;
-case "contato":
-
-require_once "view/contato.php";
-
-
-break;
-
-default:
-
-
-	require_once "view/home.php"; 
-	 
-}
-
-include "view/rodape.php";
+ require_once "view/rodape.php";
 ?>
 </body>
 </html>
