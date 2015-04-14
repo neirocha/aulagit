279c04e455671bba64e7faff204fcc7fad4929c2
neirocha <saturncomunicacao@hotmail.com>
2015-04-13 23:38:38 -0300

    Tipos de Cliente alteracao1

--- oo/CadastroCliente/public/index.php
+++ oo/CadastroCliente/public/index.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Nei Rocha
- * Date: 13/04/2015
- * Time: 21:22
- */
+require_once __DIR__ . "/../carregar.php";
+use Database\DataBase;
+$db = new DataBase("mysql:host=localhost;charset=utf8", "fase3", "root", "");
+$pdo = $db->getConnection();
+$pf = $pdo->query("SELECT * FROM PessoaFisica")->fetchAll(\PDO::FETCH_ASSOC);
+$pj = $pdo->query("SELECT * FROM PessoaJuridica")->fetchAll(\PDO::FETCH_ASSOC);
+$clientes = array_merge($pf, $pj);
+$ordem = filter_input(INPUT_GET, "ordem", FILTER_SANITIZE_STRING);
+if ($ordem === "DESC") {
+    krsort($clientes);
+}
+require_once __DIR__ . "/../src/Views/index.php";
